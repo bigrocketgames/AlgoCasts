352473dca42a654e2860fdd627b3d389a21d0863
bigrocketgames <admin@bigrocketgames.com>
2018-04-10 13:09:33 -0700

    chore: chunk complete

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,66 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  // my solution
+    // let chunkedArr = [];
+    // let count = 1;
+    // let innerArr = []
+
+    // for (let i = 0; i < array.length; i++) {
+    //   if (count === 1) {
+    //     innerArr = [(array[i])];
+        
+    //     if (i === array.length) {
+    //       chunkedArr.push(innerArr);
+    //     }
+
+    //     if (count === size) {
+    //       chunkedArr.push(innerArr);
+    //     } else {
+    //       count++;
+    //     }
+    //   } else if (count === size && i !== array.length) {
+    //     innerArr.push(array[i]);
+    //     chunkedArr.push(innerArr);
+    //     count = 1;
+    //   } else if (i === array.length - 1) {
+    //     innerArr.push(array[i]);
+    //     chunkedArr.push(innerArr);
+    //   } else {
+    //     innerArr.push(array[i]);
+    //     count++;
+    //   }
+    // }
+
+    // return chunkedArr;
+
+  // video solution #1
+    // let chunked = [];
+
+    // for (let element of array) {
+    //   const last = chunked[chunked.length - 1];
+
+    //   if (!last || last.length === size) {
+    //     chunked.push([element]);
+    //   } else {
+    //     last.push(element);
+    //   }
+    // }
+
+    // return chunked;
+
+  // video solution #2
+    let chunked = [];
+    let index = 0;
+
+    while (index < array.length) {
+      chunked.push(array.slice(index, index + size))
+      index += size;
+    }
+
+    return chunked
+
+}
 
 module.exports = chunk;
