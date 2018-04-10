241553cabc5f4b901331aee5db8f0d6e07964871
bigrocketgames <admin@bigrocketgames.com>
2018-04-09 20:12:06 -0700

    chore:  ReverseString completed.

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,21 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  // convert string to array, reverse it, and convert back to string
+    // return str.split("").reverse().join("");
+
+  // use for loop and loop through the word, adding each letter onto beginning of reversed word
+    // let reverseWord = "";
+
+    // for (let character of str) {
+    //   reverseWord = character + reverseWord;
+    // }
+
+    // return reverseWord;
+
+  // Utilizing reduce method
+    return str.split("").reduce((reverseWord, character) => character + reverseWord, "");
+}
 
 module.exports = reverse;
