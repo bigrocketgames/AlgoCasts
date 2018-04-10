e670b34d5dafc296b5ae57b0e4f3cb402eb0170f
bigrocketgames <admin@bigrocketgames.com>
2018-04-10 11:43:31 -0700

    chore:  Completed maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,47 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  // my solution
+    // let chars = {};
+    // let maxCharCount = 0;
+    // let returnChar = '';
+
+    // str.split('').map((char) => {
+    //   if (chars[char]) {
+    //     chars[char] = chars[char] + 1;
+    //   } else {
+    //     chars[char] = 1;
+    //   }
+      
+    //   if (chars[char] > maxCharCount) {
+    //     maxCharCount = chars[char]
+    //     returnChar = char;
+    //   }
+        
+    // })
+
+    // return returnChar;
+
+  // video solution
+    let charMap = {};
+    let max = 0;
+    let maxChar = '';
+
+    for (let char of str) {
+      if (charMap[char]) {
+        charMap[char]++;
+      } else {
+        charMap[char] = 1;
+      }
+
+      if (charMap[char] > max) {
+        max = charMap[char];
+        maxChar = char;
+      }
+    }
+
+    return maxChar;
+}
 
 module.exports = maxChar;
