751239d184e346d16ddf187e0ab59847322b02e6
bigrocketgames <admin@bigrocketgames.com>
2018-04-10 14:26:25 -0700

    chore: completed fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,43 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn) {
+  const cache = {};
+  
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  }
+}
+
+function fib(n) {
+  // my recursive solution
+    if (n < 2) {
+      return n
+    } else {
+      return fib(n-2) + fib(n-1)
+    }
+
+  // my iterative solution
+    // fibArr = [];
+    // for (let i = 0; i <= n; i++) {
+    //   if (i < 2) {
+    //     fibArr.push(i)
+    //   } else {
+    //     fibArr.push(fibArr[i-2] + fibArr[i-1])
+    //   }
+    // }
+
+    // return fibArr[fibArr.length - 1]
+
+}
+
+fib = memoize(fib);
 
 module.exports = fib;
