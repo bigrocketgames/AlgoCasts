26aea6c555270dd0d27abd01f20c5a5cf7e6e341
bigrocketgames <admin@bigrocketgames.com>
2018-04-10 10:44:07 -0700

    chore: ReverseInt completed

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,24 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  // my answer
+    // let reverseNumberString = n.toString().split("").reverse().join("");
+    // const sign = Math.sign(n)
+
+    // if (sign === -1) {
+    //   reverseNumberString = "-" + reverseNumberString;
+    //   return parseInt(reverseNumberString);
+    // } else if (sign === 0 || sign === 1) {
+    //   return parseInt(reverseNumberString);
+    // }
+
+  // video answer
+    const reversed = n.toString().split('').reverse().join('');
+
+    return parseInt(reversed) * Math.sign(n);
+
+
+}
 
 module.exports = reverseInt;
